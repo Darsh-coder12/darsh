d3bca3c04e168a4ced037a10d6bc9158a18c9757
whiteye <aryan14decsingh@gmail.com>
2024-05-05 19:24:26 +0530

    take 2

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
+export const SITE_TITLE = 'Darsh Agarwala | Personal Showcase Site';
 export const SITE_DESCRIPTION = 'Personal Showcase website';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
